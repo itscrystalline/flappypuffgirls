2809b715389d9fd8d6f152d15d4894047604a8b9
itscrystalline <crystal@iw2tryhard.dev>
2026-08-24 22:32:01 +0700

    score: tweak effects

--- Assets/Scripts/ScoreDisplayController.cs
+++ Assets/Scripts/ScoreDisplayController.cs
@@ -14,35 +14,35 @@ public class ScoreDisplayController : MonoBehaviour
     game = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameplayManager>();
     children = GetComponentsInChildren<TMP_Text>(true);
 
-    game.onPipePass.AddListener(OnMilestone);
-    game.onPipeCriticalPass.AddListener(OnMilestone);
+    game.onPipePass.AddListener(() => OnMilestone(5f));
+    game.onPipeCriticalPass.AddListener(() => OnMilestone(10f));
   }
 
-  void OnMilestone()
+  void OnMilestone(float increaseFontSizeBy)
   {
     var clones = children.Select(t => Instantiate(t.gameObject, transform).GetComponent<TMP_Text>()).ToArray();
-    StartCoroutine(DoScoreFade(clones));
+    StartCoroutine(DoScoreFade(clones, increaseFontSizeBy));
   }
 
-  IEnumerator DoScoreFade(TMP_Text[] texts)
+  float EaseOutQuint(float x) => 1 - Mathf.Pow(1 - x, 5);
+  IEnumerator DoScoreFade(TMP_Text[] texts, float increaseFontSizeBy)
   {
+    var startingFontSize = texts[0].fontSize;
     for (var step = 1; step <= 15; step++)
     {
       foreach (var t in texts)
       {
-        t.fontSize++;
-        t.color = new Color(t.color.r, t.color.g, t.color.b, t.color.a - 1f / 15f);
+        var ease = EaseOutQuint(step / 15f);
+        t.fontSize = startingFontSize + (ease * increaseFontSizeBy);
+        t.color = new Color(t.color.r, t.color.g, t.color.b, 1 - ease);
       }
-      yield return null;
-      yield return null;
-      yield return null;
+      yield return new WaitForFixedUpdate();
     }
     foreach (var t in texts) Destroy(t.gameObject);
-    yield return 0;
   }
 
   void Update()
   {
-    foreach (var t in children) t.text = $"{Math.Round((game.localDifficulty - 1) * 10000)}".PadLeft(12, '0');
+    foreach (var t in children) t.text = $"{Math.Round((game.localDifficulty - 1) * 10000)}".PadLeft(10, '0');
   }
 }
