8522c6dfb09a0e545c2b21a85e97cd9d42fc7340
itscrystalline <crystal@iw2tryhard.dev>
2026-08-27 19:57:50 +0700

    players: split the girls up as individual people

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,12 +7,19 @@ public class PlayerController : MonoBehaviour
   private InputAction jump;
   private GameplayManager game;
   private Rigidbody2D rb;
-  // Start is called once before the first execution of Update after the MonoBehaviour is created
-  void Start()
+
+  private (Transform, Vector2)[] girls;
+
+  void Awake()
   {
     jump = InputSystem.actions.FindAction("Jump");
     rb = gameObject.GetComponent<Rigidbody2D>();
-    game = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameplayManager>();
+    girls = transform.Cast<Transform>().Select(t => (t, CartesianToPolar(t.localPosition))).ToArray();
+  }
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start()
+  {
+    game = GameplayManager.INSTANCE;
   }
 
   // Update is called once per frame
@@ -20,12 +28,26 @@ public class PlayerController : MonoBehaviour
     var velX = game.playerSpeed;
     var velY = rb.linearVelocityY;
     var normLook = new Vector2(velX, velY).normalized;
-    var lookAngle = Mathf.Atan2(normLook.y, normLook.x) * Mathf.Rad2Deg;
-    transform.rotation = Quaternion.Euler(0, 0, lookAngle);
+    var lookAngle = Mathf.Atan2(normLook.y, normLook.x);
+
+    foreach ((var gt, var gtp) in girls)
+    {
+      gt.localPosition = PolarToCartesian(new Vector2(gtp.x, gtp.y + lookAngle * 0.35f));
+      gt.localEulerAngles = new Vector3(0, 0, lookAngle * Mathf.Rad2Deg);
+    }
 
     if (jump.WasPerformedThisFrame())
     {
       rb.linearVelocity = new Vector2(0, game.playerJumpForce);
     }
   }
+
+  /// x -> r
+  /// y -> θ
+  Vector2 CartesianToPolar(Vector2 cartisian) =>
+    new Vector2(
+      x: Mathf.Sqrt(Mathf.Pow(cartisian.x, 2) + Mathf.Pow(cartisian.y, 2)),
+      y: Mathf.Atan2(cartisian.y, cartisian.x)
+    );
+  Vector2 PolarToCartesian(Vector2 polar) => new Vector2(polar.x * Mathf.Cos(polar.y), polar.x * Mathf.Sin(polar.y));
 }
